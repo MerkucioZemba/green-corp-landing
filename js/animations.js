a71ebc343a664ab4378cddcf5208901ac2feed66
MerkucioZemba <merkucio.sama@gmail.com>
2023-03-12 14:25:28 +0300

    final_js

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,7 @@
 const INCREASE_NUMBER_ANIMATION_SPEED = 50;
 
+let animationInited = false;
+
 function increaseNumberAnimationStep (i, element, endNumber) {
   if (i <= endNumber) {
     if (i === endNumber) {
@@ -22,7 +24,6 @@ function initIncreaseNumberAnimation() {
   increaseNumberAnimationStep(0, element, 5000);
 }
 
-initIncreaseNumberAnimation();
 
 
 document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
@@ -44,3 +45,49 @@ document.querySelector('#budget').addEventListener('change', function handleSele
 	document.querySelector('#form form').removeChild(otherInput); // Задание 4
 	}
   });
+
+  
+  function updateScroll() {
+	if (window.scrollY > 0) {
+	  document.querySelector('header').classList.add('header__scrolled');
+	} else {
+	  document.querySelector('header').classList.remove('header__scrolled');
+	}
+  
+	let windowBottomPosition = window.scrollY + window.innerHeight;
+	let countElementPosition = document.querySelector(
+	  '.features__clients-count'
+	).offsetTop;
+	if (windowBottomPosition >= countElementPosition && !animationInited) {
+	  animationInited = true;
+	  initIncreaseNumberAnimation();
+	}
+  }
+  window.addEventListener('scroll', updateScroll);
+
+
+
+  function addSmoothScroll(anchor) {
+	anchor.addEventListener('click', function (e) {
+	  e.preventDefault();
+   
+	  document.querySelector(this.getAttribute('href')).scrollIntoView({
+		behavior: 'smooth'
+	  });
+	});
+  }
+   
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	addSmoothScroll(anchor);
+  });
+
+  function onLinkClick(event) {
+	event.preventDefault();
+   
+	document.querySelector(event.target.getAttribute('href')).scrollIntoView({
+	  behavior: 'smooth'
+	});
+  }
+
+  addSmoothScroll(document.querySelector('.more-button'));
+  
